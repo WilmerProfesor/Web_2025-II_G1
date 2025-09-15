a97d5d0422c511d6bf691480dea305a768ce509d
unknown <wilmerprofesor@gmail.com>
2025-09-15 08:51:16 -0500

    class React v1

--- JavaScript_1/js/javascript.js
+++ JavaScript_1/js/javascript.js
@@ -19,8 +19,8 @@ console.log(isStudent);
 let apellido = "Patino"; // String
 let altura = 1.75; // Number (Decimal)
 let isMarried = false; // Boolean
-let dato=5;
-let dato2="5";
+let dato = 5;
+let dato2 = "5";
 
 console.log(typeof apellido);
 console.log(typeof altura);
@@ -41,15 +41,76 @@ console.log("Módulo: " + (a % b));
 
 if (dato > dato2) {
     console.log("dato es mayor que dato2");
-} else {
+    // }a else {
     console.log("dato no es mayor que dato2");
 }
 
-const x=[];
-const y=[1,2,3];
+const x = [];
+const y = [1, 2, 3];
 console.log(y);
-const arreglo=[4,"kjchsdk",true,[3,4,5],{id:123,nombre:"Wilmer"}];
+const arreglo = [4, "kjchsdk", true, [3, 4, 5], { id: 123, nombre: "Wilmer" }];
 console.log(arreglo);
-arreglo.splice(1,2);
+arreglo.splice(1, 2);
 console.log(arreglo);
+
+// ================= FUNCTIONS =================
+
+function suma(a, b) {
+    return a + b;
+}
+console.log(suma(4, 5));
+
+// ================= ANONYMOUS FUNCTIONS =================
+
+const variableFuncion = function (a, b) {
+    return a + b;
+}
+
+console.log("Anonima--> " + variableFuncion(4, 5));
+
+// ================= ARROW FUNCTION =================
+
+const arrowA = () => { };
+const arrowB = a => { return a + 1 };
+const arrowC = (a, b) => { return a + b };
+const arrowD = (a, b) => a + b;
+const arrowE = (a, b) => { 
+    a++; 
+    b++; 
+    return a + b; 
+};
+
+const personaA= {
+    nombre:"Wilmer",
+    sexo: true,
+    notas:{}
+}
+
+const personaB= {
+    "nombre":"Wilmer",
+    "sexo": true,
+    "notas":{}
+}
+
+// ================= FIUNCIONES DE ORDEN SUPERIOR =================
+// RETORNA O RECIBE COMO PARÁMETRO OTRA FUNCIÓN
+
+const arreglo2 = [
+    {nombre: "juan", edad: 23},
+    {nombre: "pedro", edad: 12},
+    {nombre: "carlos", edad: 19},
+    {nombre: "jose", edad: 15},
+];
+
+const filtrados=arreglo2.filter((i)=>{return i.edad>=18});
+const sumatoria=arreglo2.reduce((ini,i)=> {return i.edad+ini}, 0);
+const encontrado= arreglo2.find(i=>i.nombre=="juan")
+console.log(filtrados);
+console.log(sumatoria);
+console.log(encontrado);
+
+
+
+
+
 
