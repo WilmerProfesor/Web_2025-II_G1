71014a104e4a683b82ec572c03547c33462f3d94
WilmerProfesor <wilmerprofesor@gmail.com>
2025-09-17 09:24:14 -0500

    Excercise Reactv2

---
+++ ReactV2/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './Componets/Footer/Footer'
+import Header from './Componets/Header/Header'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <h1>Hola mundo</h1>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
